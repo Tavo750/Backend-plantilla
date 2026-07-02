d078c734248d7071759c9532381a9823c9693c5c
Luis Enrique Rios Sosa <luis.rioss@pucp.edu.pe>
2026-07-01 19:09:24 -0500

    Validar capacidad de envios activos

--- src/main/java/com/plantilla/backend/modules/envio/service/impl/EnvioDiarioServiceImpl.java
+++ src/main/java/com/plantilla/backend/modules/envio/service/impl/EnvioDiarioServiceImpl.java
@@ -124,11 +124,14 @@ public class EnvioDiarioServiceImpl implements EnvioDiarioService {
 
         int disponible = capacidadMaxima - cantidadActual;
 
-        throw new ResponseStatusException(
-        HttpStatus.BAD_REQUEST,
-        "Capacidad insuficiente en el aeropuerto " + origen.getCodigoOaci()
-                + ". Capacidad máxima: " + capacidadMaxima
-        );
+        if (cantidadNueva > disponible) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Capacidad insuficiente en el aeropuerto " + origen.getCodigoOaci()
+                            + ". Capacidad máxima: " + capacidadMaxima
+                            + ". Disponible: " + disponible
+            );
+        }
     }
 
     @Override
